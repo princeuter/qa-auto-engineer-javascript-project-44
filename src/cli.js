da9550bd0dd06f68b6f1713b3c42e55e580bfbe3
princeuter <princeutermedic@gmail.com>
2024-05-13 18:14:32 +0300

    Проект урок 3

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+export default function name() {
+    const name = readlineSync.question('May I have your name?: ');
+    console.log(`Hello, ${name}`)
+    console.log("Welcome to the Brain Games!")
+    }
