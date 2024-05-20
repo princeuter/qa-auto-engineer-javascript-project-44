e079445a232763552243974d5a4f1bc8015e0953
princeuter <princeutermedic@gmail.com>
2024-05-20 21:34:13 +0300

    4 правки

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -22,10 +22,10 @@ for (let i = 1; i <= 3; i += 1) {
       console.log('Correct!');
       counter += 1;
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'. \n Let's try again, ${player}!`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, ${player}!`);
     }
   } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'. \n Let's try again, ${player}!`);
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, ${player}!`);
   }
 }
 if (counter === 3) console.log(`Congratulations, ${player}!`);
